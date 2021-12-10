6abb389e2823c2a94301c521edf10fa631a6ee69
BjarneMatz <bjarne.matz@gmail.com>
2021-12-10 16:09:19 +0100

    changed wheel diameter

--- main_loop/config.ino
+++ main_loop/config.ino
@@ -1,4 +1,4 @@
 //motor and wheel settings
-const float diameter = 1;//set to mm of wheel diameter
+const float diameter = 100;//set to mm of wheel diameter
 const int steps = 1600; //set to steps per revolution
 const int maxkmh = 1; //set to max speed to drive in km/h
